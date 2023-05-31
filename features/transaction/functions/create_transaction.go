163687efcb67328c6206aedea9c32ecb64042577
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-06-01 00:59:31 +0700

    fix: uncomment n

--- features/transaction/functions/create_transaction.go
+++ features/transaction/functions/create_transaction.go
@@ -15,9 +15,9 @@ func CreateTransaction(transactions *tStruct.ArrTransaction, spareparts *sStruct
 	var inputString string
 	// var inputSparepart string
 
-	// n := transactions.N + 1
+	n := transactions.N + 1
 	// i := transactions.N
-	
+
 	fmt.Println("O-----------------------------------------------O")
 	fmt.Println("|                 Tambah Transaksi              |")
 	fmt.Println("|-----------------------------------------------|")
@@ -39,45 +39,52 @@ func CreateTransaction(transactions *tStruct.ArrTransaction, spareparts *sStruct
 	// }
 
 	// Input Date
-	fmt.Print("| Masukan Tanggal : "); fmt.Scan(&inputInt)
+	fmt.Print("| Masukan Tanggal : ")
+	fmt.Scan(&inputInt)
 
 	for !common.CheckIfDateIsValid(inputInt) {
 		fmt.Println("| Tanggal tidak valid!                          |")
-		fmt.Print("| Masukan Tanggal : "); fmt.Scan(&inputInt)
+		fmt.Print("| Masukan Tanggal : ")
+		fmt.Scan(&inputInt)
 	}
 
 	transactions.Data[n].Date = inputInt
 
 	// Input Month
-	fmt.Print("| Masukan Bulan : "); fmt.Scan(&inputInt)
+	fmt.Print("| Masukan Bulan : ")
+	fmt.Scan(&inputInt)
 
 	for !common.CheckIfMonthIsValid(inputInt) {
 		fmt.Println("| Bulan tidak valid!                          |")
-		fmt.Print("| Masukan Bulan : "); fmt.Scan(&inputInt)
+		fmt.Print("| Masukan Bulan : ")
+		fmt.Scan(&inputInt)
 	}
 
 	transactions.Data[n].Month = inputInt
 
 	// Input Year
-	fmt.Print("| Masukan Tahun : "); fmt.Scan(&inputInt)
+	fmt.Print("| Masukan Tahun : ")
+	fmt.Scan(&inputInt)
 	transactions.Data[n].Year = inputInt
 
 	// Input customerId
-	fmt.Print("| Masukan ID Customer : "); fmt.Scan(&inputString)
+	fmt.Print("| Masukan ID Customer : ")
+	fmt.Scan(&inputString)
 	transactions.Data[n].CustomerId = inputString
-	
+
 	// Input paymentMethod
-	fmt.Print("| Masukan Metode Pembayaran : "); fmt.Scan(&inputString)
+	fmt.Print("| Masukan Metode Pembayaran : ")
+	fmt.Scan(&inputString)
 	transactions.Data[n].PaymentMethod = inputString
 
 	// Input paymentMethod
-	fmt.Print("| Note : "); fmt.Scan(&inputString)
+	fmt.Print("| Note : ")
+	fmt.Scan(&inputString)
 	transactions.Data[n].Note = inputString
 
 	return false
 }
 
 func inputSpareparts(transactions *tStruct.ArrTransaction) {
-	
 
 }
