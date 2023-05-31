cd7fe86204c4c3adf9cf459695d8fe4b242409e4
Yazid Al Ghozali <54970485+yazidalg@users.noreply.github.com>
2023-06-01 00:13:04 +0700

    feat: add sparepart done (#6)

--- features/sparepart/functions/add_sparepart.go
+++ features/sparepart/functions/add_sparepart.go
@@ -1,6 +1,7 @@
 package sparepart
 
 import (
+	common "bengkelin/common"
 	s "bengkelin/features/sparepart/structs"
 	"fmt"
 )
@@ -10,27 +11,24 @@ func AddSparepart(spareparts *s.ArrSparepart) {
 	fmt.Println("0-----------------------------------------------0")
 	fmt.Println("|		Tambah Data Sparepart		|")
 	fmt.Println("0-----------------------------------------------0")
-	inputSparepart(spareparts)
-	printSparepart(*spareparts)
+	if spareparts.N < s.NMAX-1 {
+		inputSparepart(spareparts)
+	} else {
+		common.ShowFullData()
+	}
 }
 
 func inputSparepart(spareparts *s.ArrSparepart) {
 	spareparts.N++
 	fmt.Print("→ Masukan Nama : ")
-	fmt.Scanf("%s", &spareparts.Data[spareparts.N-1].Name)
+	fmt.Scan(&spareparts.Data[spareparts.N-1].Name)
 	fmt.Print("→ Masukan Harga : ")
-	fmt.Scanf("%d", &spareparts.Data[spareparts.N-1].Harga)
+	fmt.Scan(&spareparts.Data[spareparts.N-1].Harga)
 	fmt.Print("→ Masukan Stok : ")
-	fmt.Scanf("%d", &spareparts.Data[spareparts.N-1].Stok)
+	fmt.Scan(&spareparts.Data[spareparts.N-1].Stok)
 	spareparts.Data[spareparts.N].Id = spareparts.N
 
 	fmt.Println("0--------------------------------------------0")
 	fmt.Println("|    Yeay! Sparepart Berhasil Ditambahkan    |")
 	fmt.Println("0--------------------------------------------0")
-}
-
-func printSparepart(spareparts s.ArrSparepart) {
-	for i := 0; i < spareparts.N; i++ {
-		fmt.Println(spareparts.Data[i].Name, spareparts.Data[i].Harga, spareparts.Data[i].Stok)
-	}
 }
