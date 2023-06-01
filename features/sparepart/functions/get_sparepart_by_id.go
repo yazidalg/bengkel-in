43a10b206323607b776bdaad0e84f02746ad887c
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-06-01 22:47:05 +0700

    onprogress: working on edit sparepart

--- features/sparepart/functions/get_sparepart_by_id.go
+++ features/sparepart/functions/get_sparepart_by_id.go
@@ -4,18 +4,10 @@ import s "bengkelin/features/sparepart/structs"
 
 func GetSparepartById(spareparts s.ArrSparepart, id int) int {
 	index := -1
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ff9c91 (onprogress: working on edit sparepart)
 	for i := 0; i < spareparts.N; i++ {
 		if id == spareparts.Data[i].Id {
 			index = i
 		}
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ff9c91 (onprogress: working on edit sparepart)
 	return index
 }
