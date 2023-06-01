fd9b919c9dddab8b664f17aba907f252c7bf175e
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-06-01 22:45:38 +0700

    fix: rebase

--- features/sparepart/functions/get_sparepart_by_id.go
+++ features/sparepart/functions/get_sparepart_by_id.go
@@ -4,12 +4,18 @@ import s "bengkelin/features/sparepart/structs"
 
 func GetSparepartById(spareparts s.ArrSparepart, id int) int {
 	index := -1
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8ff9c91 (onprogress: working on edit sparepart)
 	for i := 0; i < spareparts.N; i++ {
 		if id == spareparts.Data[i].Id {
 			index = i
 		}
 	}
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8ff9c91 (onprogress: working on edit sparepart)
 	return index
 }
