8b8c581b04524d1eabd9eb4df8ac60e8d7fbdcaf
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-06-06 00:21:49 +0700

    fix: index out of range

--- features/sparepart/functions/delete_sparepart.go
+++ features/sparepart/functions/delete_sparepart.go
@@ -9,41 +9,29 @@ import (
 func DeleteSparepart(sparepart *sStruct.ArrSparepart) {
 	common.ResetConsole()
 
-	var inputUser int
+	var inputUser, i int
+	i = -1
 	fmt.Println("============================================")
 	fmt.Println("              Hapus Sparepart               ")
 	fmt.Println("============================================")
 	ShowSparepart(*sparepart)
 	fmt.Println()
 	fmt.Println("Hapus sparepart berdasarkan ID : ")
-	fmt.Scan(&inputUser)
 
-	data := GetSparepartById(*sparepart, inputUser)
+	for i == -1 {
+		fmt.Scan(&inputUser)
+		i = GetSparepartById(*sparepart, inputUser)
 
-	DetailSparepart(&sparepart.Data[data])
+		if i == -1 {
+			fmt.Println("ID tidak ditemukan, silahkan masukan kembali ID yang ingin dihapus : ")
+		}
+	}
 
-	for i := data; i < sparepart.N-1; i++ {
+	DetailSparepart(&sparepart.Data[i])
+
+	for i := i; i < sparepart.N-1; i++ {
 		sparepart.Data[i] = sparepart.Data[i+1]
 	}
 	sparepart.N--
 
 }
-
-// N = [
-// 	0 => {
-// 		id : 1
-// 		nama : yazid
-// 	},
-// 	1 => {
-// 		id : 2
-// 		nama : wildan
-// 	},
-// 	2 => {
-// 		id : 3,
-// 		nama : indra
-// 	},
-// 	3 => {
-// 		id : 4,
-// 		nama : reinhard
-// 	}
-// ]
