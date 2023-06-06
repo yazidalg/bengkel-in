e79baf07ff30df9a209dd4693732bbc4c9b3147a
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-06-06 23:54:23 +0700

    onprogress: add condition when no data

--- features/sparepart/functions/delete_sparepart.go
+++ features/sparepart/functions/delete_sparepart.go
@@ -16,22 +16,28 @@ func DeleteSparepart(sparepart *sStruct.ArrSparepart) {
 	fmt.Println("============================================")
 	ShowSparepart(*sparepart)
 	fmt.Println()
-	fmt.Println("Hapus sparepart berdasarkan ID : ")
+	if sparepart.N == 0 {
+		fmt.Println("Wah data yang ingin dihapus masih kosong 😰")
+		fmt.Println("Coba tambah data terlebih dahulu yaa 😁")
+		fmt.Println()
+		fmt.Println()
+	} else {
+		fmt.Println("Hapus sparepart berdasarkan ID : ")
 
-	for i == -1 {
-		fmt.Scan(&inputUser)
-		i = GetSparepartById(*sparepart, inputUser)
+		for i == -1 {
+			fmt.Scan(&inputUser)
+			i = GetSparepartById(*sparepart, inputUser)
 
-		if i == -1 {
-			fmt.Println("ID tidak ditemukan, silahkan masukan kembali ID yang ingin dihapus : ")
+			if i == -1 {
+				fmt.Println("ID tidak ditemukan, silahkan masukan kembali ID yang ingin dihapus : ")
+			}
 		}
+
+		DetailSparepart(&sparepart.Data[i])
+
+		for i := i; i < sparepart.N-1; i++ {
+			sparepart.Data[i] = sparepart.Data[i+1]
+		}
+		sparepart.N--
 	}
-
-	DetailSparepart(&sparepart.Data[i])
-
-	for i := i; i < sparepart.N-1; i++ {
-		sparepart.Data[i] = sparepart.Data[i+1]
-	}
-	sparepart.N--
-
 }
